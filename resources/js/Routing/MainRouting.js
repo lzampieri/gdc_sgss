ce7983155cc55ecb1ede8a36a449c5625f30fb81
lzampieri <zampieri.leonardo98@gmail.com>
2022-03-23 23:54:23 +0100

    Initial commit

---
+++ resources/js/Routing/MainRouting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainRouting() {
+    return (
+        <div>
+            Questo è il main routing di React!
+        </div>
+    );
+}
+  
+export default MainRouting;
+  
---
+++ resources/views/react.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Fogli di stile -->
+        <link rel="stylesheet" href="{{ url('/css/app.css' ) }}" />
+
+        <!-- Caricamento (posticipato) di React -->
+        <script src="{{ url('/js/app.js' ) }}" defer></script>
+    </head>
+    <body>
+        <div id="root" />
+    </body>
+</html>
