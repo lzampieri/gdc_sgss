0f97ee0232b6828dd41efd839250e9aaa2ddd919
lzampieri <zampieri.leonardo98@gmail.com>
2022-05-14 12:20:14 +0200

    Catched TwoInvalidStateException

--- routes/auth.php
+++ routes/auth.php
@@ -4,13 +4,18 @@ use App\Http\Controllers\Users;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
- 
+use Laravel\Socialite\Two\InvalidStateException;
+
 Route::get('/auth/redirect', function () {
     return Socialite::driver('google')->scopes([ 'email' ])->redirect();
 })->name('auth.redirect');
  
 Route::get('/auth/callback', function () {
-    return Users::login( Socialite::driver('google')->user()->email );
+    try {
+        return Users::login( Socialite::driver('google')->user()->email );
+    } catch ( InvalidStateException $e ) {
+        return redirect( route('home') )->with( 'negative-message', 'C\'è stato un problema di comunicazione con Google. Riprova, per favore.');
+    }
 })->name('auth.callback');
 
 Route::get('/signup', function () { return view( 'signup' ); } )->name( 'signup' );
